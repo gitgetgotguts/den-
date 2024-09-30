30f2486cf2e947d9ed7ceb987693cf0c20adcbcb
gitgetgotguts <getgetgit@proton.me>
2024-09-30 03:53:04 +0100

    created functions to choose the important data

--- extract/get.py
+++ extract/get.py
@@ -28,12 +28,37 @@ def req_by_ingredients(ingredients,num_of_recipes,apikey,minimize_missing=0,igno
     
     return data
 
+def get_reci_info(id,apikey):
+    params={'apiKey': apikey}
+    
+    url=f'https://api.spoonacular.com/recipes/{id}/information'
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+    # Parse the JSON response
+    
+        data = response.json()
+    
+        # print(data)
+    else:
+        print(f'Error: {response.status_code}')
+    return data
+
+
+
+    
+    
+
+
+file_N='recinfo.json'
+dat=get_reci_info(991625,apikey)
+with open(file_N,'w') as json_file:
+    json.dump(dat,json_file,indent=4)
 
 
 # data=req_by_ingredients(['milk','eggs'],2,apikey)
 # # data is a list of dicts containing products
 
-# file_name='out.json'
+# file_name='out1.json'
 # with open(file_name, 'w') as json_file:
 #     json.dump(data, json_file, indent=4)
 
--- extract/transform.py
+++ extract/transform.py
@@ -1,3 +1,28 @@
-from get import req_by_ingredients,apikey
-print(req_by_ingredients(['milk','eggs'],2,apikey))
+from get import req_by_ingredients,apikey,get_reci_info
+import json
+D=req_by_ingredients(['milk','eggs'],2,apikey)
+
+def clean_by_ingredients(data) :   #recieves from req_by_ingredients
+    F=[]
+    for reciep in data:
+        dic={}
+        dic['reciep']=reciep['title']
+        dic['reciep_id']=reciep['id']
+        dic['img']=reciep['image']
+        F.append(dic)
+    return F
+def get_reciept_ingredients(data): #recieves from get_reci_info
+    steps=data['instructions']
+    reciept=data['title']
+    ingred=data['extendedIngredients']
+    ing=[x['original'] for x in ingred]
+    id=data['id']
+    dic={'name':reciept ,'id':id, 'ingredients':ing,'steps':steps}
+    
+    return dic
+dat=get_reciept_ingredients(get_reci_info(991625,apikey))
+with open('nenwfl.json','w') as json_file:
+    json.dump(dat,json_file,indent=4)
+
+
 
